1caccf850561356a226a6d9d2e2206c441cc8561
JusticeKorea <55412932+JusticeKorea@users.noreply.github.com>
2020-01-01 05:13:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -139,18 +139,19 @@ client.on("message", message => {
   if (message.content.startsWith("2")) {
     message.channel.send(```CSS🔰 [De manière générale]```);
   }
-    if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
+
+});
+
+client.on("message", async message => {
+  if (command === "say") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use.
+    // To get the "message" itself we join the `args` back into a string with spaces:
     const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
+    message.delete().catch(O_o => {});
+    // And we get the bot to say the thing:
     message.channel.send(sayMessage);
   }
-
-
-
 });
 
 client.login(process.env.TOKEN);
